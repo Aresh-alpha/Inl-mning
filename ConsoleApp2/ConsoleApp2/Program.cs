41ebe2a39da3ce04f9a6171025496f001dfb2fa3
Admin <Admin@TEBELEVDISK>
2019-10-10 09:21:18 +0200

    har fixat int varianten för att det ska räkna hur många gånger man har räknat

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -11,18 +11,29 @@ namespace ConsoleApp2
             Random random = new Random();
             int randomZ = random.Next(1 , 101);
             int Gissa;
+            int Antalgissningar = 0;
+
             do
             {
                 Gissa = int.Parse(Console.ReadLine());
+                
                 if (Gissa < randomZ)
                 {
                     Console.WriteLine("Du gissade för låg");
                 }
                 else if (Gissa > randomZ)
+                {
                     Console.WriteLine("Du gissade för hög");
+                }
+                Console.WriteLine(Antalgissningar++);
+
 
             } while (Gissa != randomZ);
-                Console.WriteLine("Grattis du gissade rätt");
+            Console.WriteLine("Grattis du gissade rätt");
+            Console.WriteLine(Antalgissningar++);
+
+
+
 
 
         }
